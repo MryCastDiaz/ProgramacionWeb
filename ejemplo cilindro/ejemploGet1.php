e2a90c5ac6d4426a75f84611d9444ae7d8eb0372
MryCastDiaz <115597871+MryCastDiaz@users.noreply.github.com>
2022-10-11 19:45:20 -0500

    Add files via upload

---
+++ ejemplo cilindro/ejemploGet1.php
@@ -0,0 +1,8 @@
+<?php //Ejemplo aprenderaprogrmar.com>
+ $diametro=$_GET['diam']
+ $altura=$_GET['altu']
+ $radio=$diametro/2;
+ $Pi=3.141593;
+ $volumen=$Pi*$radio*$altura;
+echo"</br> El volumen del cilindro es de"."$volumen"."metros cubicos";
+?>
